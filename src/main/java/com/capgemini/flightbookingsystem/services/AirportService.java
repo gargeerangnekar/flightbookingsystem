76a6b323f40d11d68a76a8c4ff398581396fc814
AmeyMac <augharge@gmail.com>
2025-05-14 15:24:04 +0530

    airport changes

---
+++ src/main/java/com/capgemini/flightbookingsystem/services/AirportService.java
@@ -0,0 +1,14 @@
+package com.capgemini.flightbookingsystem.services;
+
+import java.util.List;
+
+import com.capgemini.flightbookingsystem.entities.Airport;
+
+public interface AirportService {
+
+    Airport saveAirport(Airport airport);
+    Airport getAirportById(Long airportId);
+    List<Airport> getAllAirports();
+    Airport updateAirport(Airport airport);
+    void deleteAirport(Long airportId);
+}
---
+++ src/main/java/com/capgemini/flightbookingsystem/services/AirportServiceImpl.java
@@ -0,0 +1,52 @@
+package com.capgemini.flightbookingsystem.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.capgemini.flightbookingsystem.entities.Airport;
+import com.capgemini.flightbookingsystem.exceptions.AirportNotFoundException;
+import com.capgemini.flightbookingsystem.repositories.AirportRepository;
+
+public class AirportServiceImpl implements AirportService{
+
+	 @Autowired
+	    private AirportRepository airportRepository;
+
+	    @Override
+	    public Airport saveAirport(Airport airport) {
+	        return airportRepository.save(airport);
+	    }
+
+	    @Override
+	    public Airport getAirportById(Long airportId) {
+	        return airportRepository.findById(airportId)
+	                .orElseThrow(() -> new AirportNotFoundException("Airport ID not found :"+airportId));
+	    }
+
+	    @Override
+	    public List<Airport> getAllAirports() {
+	        return airportRepository.findAll();
+	    }
+
+	    @Override
+	    public Airport updateAirport(Airport airport) {
+	  
+	        Airport existingAirport = airportRepository.findById(airport.getAirportId())
+	                .orElseThrow(() -> new AirportNotFoundException("Airport ID not found :"+airport.getAirportId()));
+	        
+	        existingAirport.setAirportName(airport.getAirportName());
+	        existingAirport.setCity(airport.getCity());
+	        existingAirport.setContact(airport.getContact());
+	        return airportRepository.save(existingAirport);
+	    }
+
+	    @Override
+	    public void deleteAirport(Long airportId) {
+	        
+	        if (!airportRepository.existsById(airportId)) {
+	            throw new AirportNotFoundException("Airport ID not found :"+airportId);
+	        }
+	        airportRepository.deleteById(airportId);
+	    }
+}
