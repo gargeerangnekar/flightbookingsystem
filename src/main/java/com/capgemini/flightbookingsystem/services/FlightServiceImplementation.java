6d10d0d80837c17fb2caf4f05d2bb49157fe7284
RishaRane <risharane6@gmail.com>
2025-05-14 11:36:18 +0530

    Final Commit - Risha

---
+++ src/main/java/com/capgemini/flightbookingsystem/services/FlightServiceImplementation.java
@@ -0,0 +1,82 @@
+package com.capgemini.flightbookingsystem.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.capgemini.flightbookingsystem.entities.Flights;
+import com.capgemini.flightbookingsystem.exceptions.FlightNotFoundException;
+import com.capgemini.flightbookingsystem.repositories.FlightRepository;
+
+@Service
+public class FlightServiceImplementation implements FlightService {
+	
+	//Injecting repository
+	FlightRepository flightRepository;
+	
+	@Autowired
+	public FlightServiceImplementation(FlightRepository flightRepository) {
+		this.flightRepository = flightRepository;
+	}
+
+	@Override
+	public List<Flights> getAllFlights() {
+		return flightRepository.findAll();
+	}
+
+	@Override
+	public Flights getFlightById(Long flightId) {
+		return flightRepository.findById(flightId)
+				.orElseThrow(()-> new FlightNotFoundException("Flight with Id "+flightId+" is not available"));
+	}
+
+	@Override
+	public Flights createNewFlight(Flights flight) {
+		return flightRepository.save(flight);
+	}
+
+	@Override
+	public Flights updateFlightById(Long flightId, Flights flight) {
+		Flights existingFlight = flightRepository.findById(flightId)
+				.orElseThrow(()-> new FlightNotFoundException("Flight with Id "+flightId+" is not available"));
+		
+		if(flight.getFlightNumber() != null)
+			existingFlight.setFlightNumber(flight.getFlightNumber());
+		
+		if (flight.getDepartureTime() != null)
+	        existingFlight.setDepartureTime(flight.getDepartureTime());
+
+	    if (flight.getArrivalTime() != null)
+	        existingFlight.setArrivalTime(flight.getArrivalTime());
+
+	    if (flight.getStatus() != null)
+	        existingFlight.setStatus(flight.getStatus());
+
+	    if (flight.getAircraftModel() != null)
+	        existingFlight.setAircraftModel(flight.getAircraftModel());
+
+	    if (flight.getCapacity() != null)
+	        existingFlight.setCapacity(flight.getCapacity());
+
+	    if (flight.getAirlineAdminId() != null)
+	        existingFlight.setAirlineAdminId(flight.getAirlineAdminId());
+
+	    if (flight.getArrivalAirportId() != null)
+	        existingFlight.setArrivalAirportId(flight.getArrivalAirportId());
+
+	    if (flight.getDepartureAirportId() != null)
+	        existingFlight.setDepartureAirportId(flight.getDepartureAirportId());
+
+	    return flightRepository.save(existingFlight);
+	}
+
+	@Override
+	public void deleteFlight(Long flightId) {
+		Flights existingFlight = flightRepository.findById(flightId)
+				.orElseThrow(()-> new FlightNotFoundException("Flight with Id "+flightId+" not found"));
+		
+		flightRepository.delete(existingFlight);
+	}
+
+}
