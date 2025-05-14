673bde0f06b1eccfad18224ae038b11aa9128685
AmeyMac <augharge@gmail.com>
2025-05-14 15:24:04 +0530

    airport changes

---
+++ src/main/java/com/capgemini/flightbookingsystem/repositories/AirportRepository.java
@@ -0,0 +1,11 @@
+package com.capgemini.flightbookingsystem.repositories;
+
+
+	import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.capgemini.flightbookingsystem.entities.Airport;
+
+	public interface AirportRepository extends JpaRepository<Airport, Long> {
+	    // Custom queries (if needed)
+	    Airport findByAirportName(String airportName);
+	}
