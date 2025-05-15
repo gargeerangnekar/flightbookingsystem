8b8a2753b65b2a8815f0a11e6e9c6bf399e6ad45
atharvapimple3 <atharvapimple3@gmail.com>
2025-05-15 22:07:17 +0530

    Security added.

---
+++ src/main/java/com/capgemini/flightbookingsystem/exceptions/EmailNotFoundException.java
@@ -0,0 +1,9 @@
+package com.capgemini.flightbookingsystem.exceptions;
+
+public class EmailNotFoundException extends RuntimeException {
+
+	public EmailNotFoundException(String message) {
+		super(message);
+	}
+
+}
