9bc7a4ed21b353831cc78cdbe4c438a69bc63595
atharvapimple3 <atharvapimple3@gmail.com>
2025-05-15 12:33:35 +0530

    added logs to service

--- src/main/java/com/capgemini/flightbookingsystem/services/BookingServiceImpl.java
+++ src/main/java/com/capgemini/flightbookingsystem/services/BookingServiceImpl.java
@@ -8,7 +8,10 @@ import com.capgemini.flightbookingsystem.entities.Booking;
 import com.capgemini.flightbookingsystem.exceptions.BookingNotFoundException;
 import com.capgemini.flightbookingsystem.repositories.BookingRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class BookingServiceImpl implements BookingService {
 
 	BookingRepository bookingRepository;
@@ -19,25 +22,35 @@ public class BookingServiceImpl implements BookingService {
 
 	@Override
 	public void deleteBooking(Integer bookingId) {
-		Booking newBooking = bookingRepository.findById(bookingId)
-				.orElseThrow(() -> new BookingNotFoundException("Booking not found with ID:" + bookingId));
+		log.debug("Deleting booking with ID :", bookingId);
+		Booking newBooking = bookingRepository.findById(bookingId).orElseThrow(() -> {
+			log.warn("Booking not found with ID :", bookingId);
+			return new BookingNotFoundException("Booking not found with ID:" + bookingId);
+		});
+		log.info("Booking deleted successfully with ID: ", bookingId);
 		bookingRepository.delete(newBooking);
 	}
 
 	@Override
 	public List<Booking> getAllBookings() {
+		log.debug("Fetching all bookings from repository");
 		return bookingRepository.findAll();
 	}
 
 	@Override
 	public Booking saveBooking(Booking booking) {
+		log.debug("Creating new booking and saving in repository");
 		return bookingRepository.save(booking);
 	}
 
 	@Override
 	public Booking updateBooking(Integer bookingId, Booking booking) {
-		Booking existing = bookingRepository.findById(bookingId)
-				.orElseThrow(() -> new BookingNotFoundException("Booking not found with ID :" + bookingId));
+		log.debug("Updating Booking with ID: {}", bookingId);
+
+		Booking existing = bookingRepository.findById(bookingId).orElseThrow(() -> {
+			log.warn("Booking not found with ID: {}", bookingId);
+			return new BookingNotFoundException("Booking not found with ID: " + bookingId);
+		});
 
 		existing.setAmount(booking.getAmount());
 		existing.setBookingTime(booking.getBookingTime());
@@ -47,14 +60,17 @@ public class BookingServiceImpl implements BookingService {
 		existing.setFlights(booking.getFlights());
 		existing.setUsers(booking.getUsers());
 
-		return bookingRepository.save(existing);
-
+		Booking updated = bookingRepository.save(existing);
+		log.info("Booking updated successfully with ID: {}", bookingId);
+		return updated;
 	}
 
 	@Override
 	public Booking getBookingById(Integer bookingId) {
-		return bookingRepository.findById(bookingId)
-				.orElseThrow(() -> new BookingNotFoundException("Booking not found with ID :" + bookingId));
+		log.debug("Fetching booking with ID: ", bookingId);
+		return bookingRepository.findById(bookingId).orElseThrow(() -> {
+			log.warn("Booking not found with ID ", bookingId);
+			return new BookingNotFoundException("Booking not found with ID :" + bookingId);
+		});
 	}
-
 }
