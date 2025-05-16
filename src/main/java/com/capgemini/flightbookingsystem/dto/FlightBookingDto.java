c176338f8d06d6f23b264e82b7debb6615aa766b
atharvapimple3 <atharvapimple3@gmail.com>
2025-05-16 16:32:44 +0530

    removed status

---
+++ src/main/java/com/capgemini/flightbookingsystem/dto/FlightBookingDto.java
@@ -0,0 +1,15 @@
+package com.capgemini.flightbookingsystem.dto;
+
+import java.time.LocalDateTime;
+
+public class FlightBookingDto {
+	
+	private Integer departureAirportId;
+	private Integer arrivalAirportId;
+	private String airportName;
+	private LocalDateTime departureTime;
+	private LocalDateTime arrivalTime;
+	private Double amount;
+	
+
+}
