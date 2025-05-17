4064bd2f547c0a1c648127423bdcb5495c4ea5e9
atharvapimple3 <atharvapimple3@gmail.com>
2025-05-17 15:16:06 +0530

    2nd Dto updated

--- src/main/java/com/capgemini/flightbookingsystem/repositories/BookingRepository.java
+++ src/main/java/com/capgemini/flightbookingsystem/repositories/BookingRepository.java
@@ -1,9 +1,11 @@
 package com.capgemini.flightbookingsystem.repositories;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.capgemini.flightbookingsystem.dto.BookingHistoryDto;
@@ -20,9 +22,23 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
 		       "f.departureTime, f.arrivalTime, f.amount) " +
 		       "FROM Flights f " +
 		       "JOIN Airport dep ON f.departureAirportId = dep.airportId " +
-		       "JOIN Airport arr ON f.arrivalAirportId = arr.airportId "
-		       )
-		List<FlightBookingDto> getAllBookingDto();
+		       "JOIN Airport arr ON f.arrivalAirportId = arr.airportId ")
+		List<FlightBookingDto> getAllBookingDtoForDisplay();
+	
+	@Query("SELECT new com.capgemini.flightbookingsystem.dto.FlightBookingDto(" +
+		       "f.departureAirportId, f.arrivalAirportId, " +
+		       "dep.airportName, arr.airportName, "
+		       + "dep.city, arr.city, " +
+		       "f.departureTime, f.arrivalTime, f.amount) " +
+		       "FROM Flights f " +
+		       "JOIN Airport dep ON f.departureAirportId = dep.airportId " +
+		       "JOIN Airport arr ON f.arrivalAirportId = arr.airportId "+
+		       "WHERE DATE(f.departureTime) = ?1 "
+		       + "AND dep.city = :departureCity "
+		       + "AND arr.city = :arrivalCity") 
+		List<FlightBookingDto> getAllBookingDto(LocalDateTime departureTime,
+				@Param("departureCity") String departureCity,
+			    @Param("arrivalCity") String arrivalCity);
 	
 	
 	@Query("SELECT new com.capgemini.flightbookingsystem.dto.BookingHistoryDto( " +
