692b3d46d2bea19f6930b6c2c77ee96c8e118993
atharvapimple3 <atharvapimple3@gmail.com>
2025-05-18 17:49:35 +0530

    delete dto (FightBookingDto) and add test cases

--- src/main/java/com/capgemini/flightbookingsystem/repositories/BookingRepository.java
+++ src/main/java/com/capgemini/flightbookingsystem/repositories/BookingRepository.java
@@ -1,5 +1,6 @@
 package com.capgemini.flightbookingsystem.repositories;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -9,37 +10,27 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.capgemini.flightbookingsystem.dto.BookingHistoryDto;
-import com.capgemini.flightbookingsystem.dto.FlightBookingDto;
 import com.capgemini.flightbookingsystem.entities.Booking;
+import com.capgemini.flightbookingsystem.entities.Flights;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 	
-	@Query("SELECT new com.capgemini.flightbookingsystem.dto.FlightBookingDto(" +
-		       "f.departureAirportId, f.arrivalAirportId, " +
-		       "dep.airportName, arr.airportName, "
-		       + "dep.city, arr.city, " +
-		       "f.departureTime, f.arrivalTime, f.amount) " +
-		       "FROM Flights f " +
-		       "JOIN Airport dep ON f.departureAirportId = dep.airportId " +
-		       "JOIN Airport arr ON f.arrivalAirportId = arr.airportId ")
-		List<FlightBookingDto> getAllBookingDtoForDisplay();
-	
-	@Query("SELECT new com.capgemini.flightbookingsystem.dto.FlightBookingDto(" +
-		       "f.departureAirportId, f.arrivalAirportId, " +
-		       "dep.airportName, arr.airportName, "
-		       + "dep.city, arr.city, " +
-		       "f.departureTime, f.arrivalTime, f.amount) " +
-		       "FROM Flights f " +
-		       "JOIN Airport dep ON f.departureAirportId = dep.airportId " +
-		       "JOIN Airport arr ON f.arrivalAirportId = arr.airportId "+
-		       "WHERE DATE(f.departureTime) = ?1 "
-		       + "AND dep.city = :departureCity "
-		       + "AND arr.city = :arrivalCity") 
-		List<FlightBookingDto> getAllBookingDto(LocalDateTime departureTime,
-				@Param("departureCity") String departureCity,
-			    @Param("arrivalCity") String arrivalCity);
-	
+
+	@Query("""
+		    SELECT f
+		    FROM Flights f
+		    WHERE f.departureAirportId = :departureAirportId
+		      AND f.arrivalAirportId = :arrivalAirportId
+		      AND DATE(f.departureTime) = :departureDate
+		      AND f.displayStatus = 'available'
+		""")
+		List<Flights> searchFlights(
+		    @Param("departureAirportId") Integer departureAirportId,
+		    @Param("arrivalAirportId") Integer arrivalAirportId,
+		    @Param("departureDate") LocalDate departureDate
+		);
+
 	
 	@Query("SELECT new com.capgemini.flightbookingsystem.dto.BookingHistoryDto( " +
 		       "u.name, f.flightNumber, f.arrivalTime, " +
